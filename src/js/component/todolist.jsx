4eebff9e5d0baaa68bf4d71d9fdda07e70a56ff3
Natalia <nlopezsalas@gmail.com>
2024-02-10 20:12:45 +0000

    Finish the ToDo List front-end

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
-import { faX } from "@fortawesome/free-solid-svg-icons"; // Import the specific icon
+import { faX } from "@fortawesome/free-solid-svg-icons"; 
 
-let nextId = 0;
 const TodoList = () => {
 	//declaración de estados
 	const [task, setTask] = useState("");
@@ -39,27 +37,27 @@ const TodoList = () => {
 				</ul>
 				<div>{todoList.length} item left</div>
 			</div> */}
-
-			<div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+			<h1 className="text-danger-emphasis text-center mt-5 fw-light">todos</h1>
+			<div className="d-flex flex-column flex-md-row p-4 gap-4 align-items-center justify-content-center fw-light">
 				<div className="todo-list list-group rounded-0 shadow ">
 					<a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
 						<div className="d-flex gap-2 w-100 justify-content-between">
-							<form onSubmit={addTask}>
-								<input className="border border-0" type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+							<form className="w-100 row m-auto" onSubmit={addTask}>
+								<input className="border border-0 form-control form-control-lg fw-light" type="text" value={task} placeholder={(todoList.length === 0) ? "No tasks, add a task": ""} onChange={(e) => setTask(e.target.value)} />
 							</form>
 						</div>
 					</a>
 					{todoList.map((item, index) =>
-						<a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
+						<a href="#" className="task-info list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
 							<div key={index} className="d-flex gap-2 w-100 justify-content-between">
-								<div>{item}</div>
-								<div className="text-danger" onClick={(e) => deleteTask(index)}><FontAwesomeIcon icon={faX} /></div>
+								<div className="">{item}</div>
+								<div className="delete-task text-danger" onClick={(e) => deleteTask(index)}>< FontAwesomeIcon icon={faX} /></div>
 							</div>
 						</a>
 					)}
 					<a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
 						<div className="d-flex gap-2 w-100 justify-content-between">
-							<div className="text-black-50">{todoList.length} item left</div>
+							<div className="text-black-50" style={{ fontSize: "12px" }}>{todoList.length} item left</div>
 						</div>
 					</a>
 				</div>
