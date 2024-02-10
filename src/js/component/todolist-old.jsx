03b96c7bfeccdc1a85ff30edfa220b7d9c0c50da
Natalia <nlopezsalas@gmail.com>
2024-02-10 20:30:38 +0000

    fix some warnings

--- src/js/component/todolist-old.jsx
+++ src/js/component/todolist-old.jsx
@@ -6,12 +6,18 @@ const TodoList = () => {
 	//declaración de estados
 	const [task, setTask] = useState("");
 	const [todoList, setTodoList] = useState([]);
+	// function addTask(e) {
+	// 	if (e.key === "Enter") {
+	// 		setTodoList(todoList.concat(task));
+	// 		setTask("");
+	// 	}
+	// }
 
 	function addTask(e) {
 		e.preventDefault(); // Prevent form submission
-		if (task.trim() !== "") { 
-			setTodoList(todoList.concat(task)); 
-			setTask(""); 
+		if (task.trim() !== "") { // Check if the task is not empty
+			setTodoList(todoList.concat(task)); // Add task to the todo list
+			setTask(""); // Clear the input field
 		}
 	}
 	function deleteTask(index) {
@@ -20,7 +26,18 @@ const TodoList = () => {
 	}
 	return (
 		<>
-			<h1 className="todo-title text-danger-emphasis text-center mt-5 fw-light">todos</h1>
+			{/* <div className="container-fluid m-auto text-center">
+				<input type="text" onKeyDown={addTask} onChange={(e) => setTask(e.target.value)} />
+				<ul>
+					{todoList.map((item, index) =>
+						<li key={index}>
+							{item}
+							<span className="p-5" onClick={(e) => deleteTask(index)}>x</span>
+						</li>)}
+				</ul>
+				<div>{todoList.length} item left</div>
+			</div> */}
+			<h1 className="text-danger-emphasis text-center mt-5 fw-light">todos</h1>
 			<div className="d-flex flex-column flex-md-row p-4 gap-4 align-items-center justify-content-center fw-light">
 				<div className="todo-list list-group rounded-0 shadow ">
 					<a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
@@ -31,8 +48,8 @@ const TodoList = () => {
 						</div>
 					</a>
 					{todoList.map((item, index) =>
-						<a href="#" key={index} className="task-info list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
-							<div className="d-flex gap-2 w-100 justify-content-between">
+						<a href="#" className="task-info list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
+							<div key={index} className="d-flex gap-2 w-100 justify-content-between">
 								<div className="">{item}</div>
 								<div className="delete-task text-danger" onClick={(e) => deleteTask(index)}>< FontAwesomeIcon icon={faX} /></div>
 							</div>
