e4ca1c24ad65e9753a42774ec6448a74c13b795e
Natalia <nlopezsalas@gmail.com>
2024-02-13 18:23:32 +0000

    fix input issues with a element

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -7,15 +7,23 @@ const TodoList = () => {
 	const [task, setTask] = useState("");
 	const [todoList, setTodoList] = useState([]);
 
+	// function addTask(e) {
+	// 	e.preventDefault(); // Prevent form submission
+	// 	if (task.trim() !== "") {
+	// 		setTodoList(todoList.concat(task));
+	// 		setTask("");
+	// 	}
+	// }
+
 	function addTask(e) {
-		e.preventDefault(); // Prevent form submission
-		if (task.trim() !== "") {
+		if (e.key === "Enter") {
 			setTodoList(todoList.concat(task));
 			setTask("");
 		}
 	}
+	
 	function deleteTask(index) {
-		const updatedTodoList = todoList.filter((_, i) => i !== index);
+		const updatedTodoList = todoList.filter((item, i) => i !== index);
 		setTodoList(updatedTodoList);
 	}
 	return (
@@ -23,13 +31,13 @@ const TodoList = () => {
 			<h1 className="todo-title text-danger-emphasis text-center mt-5 fw-light">todos</h1>
 			<div className="d-flex flex-column flex-md-row p-4 gap-4 align-items-center justify-content-center fw-light pb-0">
 				<div className="todo-list list-group rounded-0 shadow ">
-					<a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border border-light" aria-current="true">
+					<span href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3 border border-light" aria-current="true">
 						<div className="d-flex gap-2 w-100 justify-content-between">
-							<form className="w-100 row m-auto" onSubmit={addTask}>
-								<input className="border border-0 form-control form-control-lg fw-light" type="text" value={task} placeholder={(todoList.length === 0) ? "No tasks, add a task" : "What needs to be done?"} onChange={(e) => setTask(e.target.value)} />
-							</form>
+							{/* <form className="w-100 row m-auto" onSubmit={addTask}> */}
+								<input className="border border-0 form-control form-control-lg fw-light" type="text" value={task} placeholder={(todoList.length === 0) ? "No tasks, add a task" : "What needs to be done?"} onKeyDown={addTask} onChange={(e) => setTask(e.target.value)} />
+							{/* </form> */}
 						</div>
-					</a>
+					</span>
 					{todoList.map((item, index) =>
 						<a href="#" key={index} className="task-info list-group-item list-group-item-action d-flex gap-3 py-3 border-light" aria-current="true">
 							<div className="d-flex gap-2 w-100 justify-content-between">
